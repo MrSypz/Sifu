0167373699b31774c4541752c85d1e6a045ed8c3
chai <chainot@hotmail.co.th>
2024-06-04 23:58:38 +0700

    template

--- src/main/java/sypztep/sifu/common/init/ModItems.java
+++ src/main/java/sypztep/sifu/common/init/ModItems.java
@@ -2,9 +2,16 @@ package sypztep.sifu.common.init;
 
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.SmithingTemplateItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import sypztep.sifu.Sifu;
+
+import java.util.List;
 
 public class ModItems {
     public static Item WARDENRITE_FRAGMENT;
@@ -24,5 +31,28 @@ public class ModItems {
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Sifu.id(name), item);
+    }
+    private static Item createWardenriteUpgradeSmithingTemplate() {
+        return new SmithingTemplateItem(
+                Text.translatable(Util.createTranslationKey("item", new Identifier(Sifu.MODID, "smithing_template.warden_upgrade.applies_to"))).formatted(Formatting.BLUE),
+                Text.translatable(Util.createTranslationKey("item", new Identifier(Sifu.MODID, "smithing_template.warden_upgrade.ingredients"))).formatted(Formatting.BLUE),
+                Text.translatable(Util.createTranslationKey("upgrade", new Identifier(Sifu.MODID, "warden_upgrade"))).formatted(Formatting.GRAY),
+                Text.translatable(Util.createTranslationKey("item", new Identifier(Sifu.MODID, "smithing_template.warden_upgrade.base_slot_description"))),
+                Text.translatable(Util.createTranslationKey("item", new Identifier(Sifu.MODID, "smithing_template.warden_upgrade.additions_slot_description"))), getWardenriteBaseSlotTextures(), getWardenriteAdditionsSlotTextures());
+    }
+    private static List<Identifier> getWardenriteBaseSlotTextures() {
+        return List.of(new Identifier("item/empty_armor_slot_helmet"),
+                new Identifier("item/empty_armor_slot_chestplate"),
+                new Identifier("item/empty_armor_slot_leggings"),
+                new Identifier("item/empty_armor_slot_boots"),
+                new Identifier("item/empty_slot_sword"),
+                new Identifier("item/empty_slot_pickaxe"),
+                new Identifier("item/empty_slot_axe"),
+                new Identifier("item/empty_slot_shovel"),
+                new Identifier("item/empty_slot_hoe"));
+    }
+
+    private static List<Identifier> getWardenriteAdditionsSlotTextures() {
+        return List.of(new Identifier(Sifu.MODID, "item/empty_slot_reinforced_echo_shard"));
     }
 }
