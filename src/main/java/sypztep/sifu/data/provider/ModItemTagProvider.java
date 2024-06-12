c3bd67c71ae8dd1065ebd31eebd8c6e322f80630
chai <chainot@hotmail.co.th>
2024-06-12 12:52:28 +0700

    add advanment and add item to make it enchantable

--- src/main/java/sypztep/sifu/data/provider/ModItemTagProvider.java
+++ src/main/java/sypztep/sifu/data/provider/ModItemTagProvider.java
@@ -15,6 +15,27 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        //Items
+        getOrCreateTagBuilder(ItemTags.SHOVELS)
+                .add(ModItems.WARDENRITE_SHOVEL);
+        getOrCreateTagBuilder(ItemTags.AXES)
+                .add(ModItems.WARDENRITE_AXE);
+        getOrCreateTagBuilder(ItemTags.HOES)
+                .add(ModItems.WARDENRITE_HOE);
+        getOrCreateTagBuilder(ItemTags.PICKAXES)
+                .add(ModItems.WARDENRITE_PICKAXE);
+
+
+        //Armor
+        getOrCreateTagBuilder(ItemTags.HEAD_ARMOR)
+                .add(ModItems.WARDENRITE_HELMET);
+        getOrCreateTagBuilder(ItemTags.CHEST_ARMOR)
+                .add(ModItems.WARDENRITE_CHESTPLATE);
+        getOrCreateTagBuilder(ItemTags.LEG_ARMOR)
+                .add(ModItems.WARDENRITE_LEGGINGS);
+        getOrCreateTagBuilder(ItemTags.FOOT_ARMOR)
+                .add(ModItems.WARDENRITE_BOOTS);
+
         //Trimable
         getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
                 .add(ModItems.WARDENRITE_LEGGINGS,ModItems.WARDENRITE_CHESTPLATE, ModItems.WARDENRITE_BOOTS,ModItems.WARDENRITE_HELMET);
