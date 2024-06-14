c12d33107238ced4a231eaee4b15c292a7f032d8
chai <chainot@hotmail.co.th>
2024-06-14 14:11:31 +0700

    What?

---
+++ src/main/java/sypztep/sifu/common/item/WardenriteSword.java
@@ -0,0 +1,10 @@
+package sypztep.sifu.common.item;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class WardenriteSword extends SwordItem {
+    public WardenriteSword(ToolMaterial toolMaterial, Settings settings) {
+        super(toolMaterial, settings);
+    }
+}
