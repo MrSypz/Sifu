0ed1fe56e6640cf251008e58c52e10155880853a
chai <chainot@hotmail.co.th>
2024-06-14 13:34:23 +0700

    Custom 3D Render when hold

--- src/main/java/sypztep/sifu/client/render/item/CustomRenderer.java
+++ src/main/java/sypztep/sifu/client/render/item/CustomRenderer.java
@@ -1,31 +1,32 @@
 package sypztep.sifu.client.render.item;
 
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import sypztep.sifu.common.init.ModItems;
-import sypztep.sifu.common.item.Warfan;
 
 public class CustomRenderer {
-    public static void init(){
-//        for (Warfan item : ModItems.ALL_WARFAN) {
-//            Identifier id = Registries.ITEM.getId(item);
-//
-//            CustomItemRenderer customItemRenderer = new CustomItemRenderer(id);
-//            ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(customItemRenderer);
-//            BuiltinItemRendererRegistry.INSTANCE.register(item, customItemRenderer);
-//            ModelLoadingRegistry.INSTANCE.registerModelProvider(((manager, out) -> {
-//                out.accept(new ModelIdentifier(id.getNamespace(),id.getPath() + "_gui","inventory"));
-//                out.accept(new ModelIdentifier(id.getNamespace(),id.getPath() + "_handheld","inventory"));
-//            }));
-//        }
+    public CustomRenderer() {
+
     }
-//    public CustomRenderer() {}
+    public static void init() {
+        for (Item item : ModItems.ALL_CUSTOM3D) {
+            Identifier id = Registries.ITEM.getId(item);
+
+            CustomItemRenderer customItemRenderer = new CustomItemRenderer(id);
+            ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(customItemRenderer);
+            BuiltinItemRendererRegistry.INSTANCE.register(item, customItemRenderer);
+            ModelLoadingRegistry.INSTANCE.registerModelProvider(((manager, out) -> {
+                out.accept(new ModelIdentifier(id.getNamespace(), id.getPath() + "_gui", "inventory"));
+                out.accept(new ModelIdentifier(id.getNamespace(), id.getPath() + "_handheld", "inventory"));
+            }));
+        }
+    }
 //    @Override
 //    public void onInitializeModelLoader(Context pluginContext) {
 //        for (Warfan item : ModItems.ALL_WARFAN) {
