cf55d2fc2f2f34b49bf136657dda6c5e34a6d040
chai <chainot@hotmail.co.th>
2024-06-16 04:44:58 +0700

    give up

--- src/main/java/sypztep/sifu/client/SifuClient.java
+++ src/main/java/sypztep/sifu/client/SifuClient.java
@@ -1,12 +1,9 @@
 package sypztep.sifu.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.util.ModelIdentifier;
-import sypztep.sifu.Sifu;
 import sypztep.sifu.client.payload.AddWardenriteArmorParticlesPayload;
 import sypztep.sifu.client.render.entity.NeedleEntityRenderer;
 import sypztep.sifu.client.render.entity.PortalizeEntityRenderer;
@@ -23,9 +20,6 @@ public class SifuClient implements ClientModInitializer {
         EntityRendererRegistry.register(ModEntityTypes.PORTALIZE, PortalizeEntityRenderer::new);
 
         EntityModelLayerRegistry.registerModelLayer(ShadowShardModel.MODEL_LAYER, ShadowShardModel::getTexturedModelData);
-        ModelLoadingPlugin.register(pluginContext -> {
-            pluginContext.addModels(ModelIdentifier.ofInventoryVariant(Sifu.id("wardenrite_cleaver_handheld")).id());
-        });
     }
 
     private void initPayloads() {
