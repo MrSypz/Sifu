032395f4630a13f62a0959886b417e1c115c9b54
chai <chainot@hotmail.co.th>
2024-06-16 03:33:46 +0700

    Now it have own damagetype

--- src/main/java/sypztep/sifu/client/SifuClient.java
+++ src/main/java/sypztep/sifu/client/SifuClient.java
@@ -1,15 +1,21 @@
 package sypztep.sifu.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
+import sypztep.sifu.Sifu;
 import sypztep.sifu.client.payload.AddWardenriteArmorParticlesPayload;
 import sypztep.sifu.client.render.entity.NeedleEntityRenderer;
 import sypztep.sifu.client.render.entity.PortalizeEntityRenderer;
 import sypztep.sifu.client.render.entity.ShadowShardsEntityRenderer;
 import sypztep.sifu.client.render.model.ShadowShardModel;
 import sypztep.sifu.common.init.ModEntityTypes;
+
+import java.util.Collection;
 
 public class SifuClient implements ClientModInitializer {
     @Override
@@ -20,6 +26,9 @@ public class SifuClient implements ClientModInitializer {
         EntityRendererRegistry.register(ModEntityTypes.PORTALIZE, PortalizeEntityRenderer::new);
 
         EntityModelLayerRegistry.registerModelLayer(ShadowShardModel.MODEL_LAYER, ShadowShardModel::getTexturedModelData);
+        ModelLoadingPlugin.register(pluginContext -> {
+            pluginContext.addModels((Collection<? extends Identifier>) MinecraftClient.getInstance().getBakedModelManager().getModel(Sifu.id("item/wardenrite_cleaver_handheld")));
+        });
     }
 
     private void initPayloads() {
