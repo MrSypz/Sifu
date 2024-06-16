5b957800844fe23f1bbb7bb69bcea9150172ae21
chai <chainot@hotmail.co.th>
2024-06-16 13:23:51 +0700

    Remove Desc

--- src/main/java/sypztep/sifu/common/item/impl/WardenriteWarfan.java
+++ src/main/java/sypztep/sifu/common/item/impl/WardenriteWarfan.java
@@ -4,7 +4,6 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.OminousItemSpawnerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
@@ -19,13 +18,11 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import sypztep.sifu.Sifu;
 import sypztep.sifu.common.entity.projectile.PortalizeEntity;
 import sypztep.sifu.common.entity.projectile.ShadowShardsEntity;
 import sypztep.sifu.common.item.Warfan;
-import sypztep.sifu.common.util.ItemDescriptionHandler;
 import sypztep.sifu.common.util.RaytraceUtil;
 
 import java.util.List;
@@ -35,22 +32,11 @@ public class WardenriteWarfan extends Warfan {
     private static final int MAX_SOUL_POINTS = 20;
     private static final int PARTICLE_COUNT = 12;
     private static final int SHARD_COUNT = 9;
-    private final ItemDescriptionHandler descriptionHandler = new ItemDescriptionHandler();
 
     public WardenriteWarfan(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings);
-        setupDescriptions();
     }
 
-    private void setupDescriptions() {
-        descriptionHandler.addDescription("This item provides a passive effect.", ItemDescriptionHandler.DescriptionType.PASSIVE);
-        descriptionHandler.addDescription("This item triggers a special effect on hit.", ItemDescriptionHandler.DescriptionType.ON_HIT);
-        descriptionHandler.addDescription("This item has a unique special ability.", ItemDescriptionHandler.DescriptionType.SPECIAL);
-        descriptionHandler.addDescription("Lore:  In shadows deep, the warfan weeps,", ItemDescriptionHandler.DescriptionType.LORE);
-        descriptionHandler.addDescription("A soul ensnared in chaos keeps.", ItemDescriptionHandler.DescriptionType.LORE);
-        descriptionHandler.addDescription("Gods watch above as death draws near", ItemDescriptionHandler.DescriptionType.LORE);
-        descriptionHandler.addDescription("A cursed breath, eternal fear.", ItemDescriptionHandler.DescriptionType.LORE);
-    }
 
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
@@ -58,21 +44,17 @@ public class WardenriteWarfan extends Warfan {
         NbtCompound nbt = getNbtCompound(stack);
         int soulCount = nbt.getInt(SOUL_KEY);
 
-        // Show lore only when Shift is held
-        if (Screen.hasShiftDown()) {
-            tooltip.addAll(descriptionHandler.getDescriptions());
-        } else {
-            tooltip.add(Text.literal("Press Shift for detail").formatted(Formatting.GRAY).formatted(Formatting.ITALIC));
-            tooltip.add(Text.literal("Soul Collector: ").formatted(Formatting.YELLOW)
-                    .append(Text.literal(String.valueOf(soulCount)).formatted(Formatting.RED))
-                    .formatted(Formatting.ITALIC));
-        }
+        tooltip.add(Text.literal("Soul Collector: ").formatted(Formatting.YELLOW)
+                .append(Text.literal(String.valueOf(soulCount)).formatted(Formatting.RED))
+                .formatted(Formatting.ITALIC));
     }
+
     @Override
     public void onCraftByPlayer(ItemStack stack, World world, PlayerEntity player) {
         stack.apply(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT, comp -> comp.apply(itemnbt -> itemnbt.putInt(SOUL_KEY, 0)));
         super.onCraftByPlayer(stack, world, player);
     }
+
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
@@ -105,6 +87,7 @@ public class WardenriteWarfan extends Warfan {
         }
         return super.postHit(stack, target, attacker);
     }
+
     private void targetParticle(World world, LivingEntity target) {
         if (target != null) {
             for (int i = 0; i < PARTICLE_COUNT; i++) {
@@ -117,11 +100,12 @@ public class WardenriteWarfan extends Warfan {
             }
         }
     }
+
     private void spawnShards(World world, PlayerEntity user) {
         for (int i = 0; i < SHARD_COUNT; i++) {
-            ShadowShardsEntity shardsEntity = new ShadowShardsEntity(world, user, 6,null);
+            ShadowShardsEntity shardsEntity = new ShadowShardsEntity(world, user, 6, null);
             shardsEntity.setOwner(user);
-            PortalizeEntity entity = PortalizeEntity.create(world,shardsEntity,user);
+            PortalizeEntity entity = PortalizeEntity.create(world, shardsEntity, user);
             double angle = 2 * Math.PI * i / SHARD_COUNT;
             double radius = 2.0;
             double xOffset = radius * Math.cos(angle);
