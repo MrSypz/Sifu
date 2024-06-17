c37d470f273792a7e9171e92fb9ad3e0ab59a337
chai <chainot@hotmail.co.th>
2024-06-18 01:27:43 +0700

    More SOund!

--- src/main/java/sypztep/sifu/common/init/ModSoundEvents.java
+++ src/main/java/sypztep/sifu/common/init/ModSoundEvents.java
@@ -13,8 +13,9 @@ public interface ModSoundEvents {
 	Map<SoundEvent, Identifier> SOUND_EVENTS = new LinkedHashMap<>();
 	//Sound
 	SoundEvent ENTITY_STYGIA = createSoundEvent("entity.stygia");
-
-
+	SoundEvent ENTITY_HIKE = createSoundEvent("entity.hike");
+	SoundEvent ENTITY_SHOT = createSoundEvent("entity.shot");
+	SoundEvent ENTITY_FLYHOOK = createSoundEvent("entity.flyhook");
     static void init() {
 		SOUND_EVENTS.keySet().forEach((soundEvent) -> {
 			Registry.register(Registries.SOUND_EVENT, SOUND_EVENTS.get(soundEvent), soundEvent);
